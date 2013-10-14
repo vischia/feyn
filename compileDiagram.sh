4cafb4b4c59a6ef82092212cbcad055bf272264b
Pietro Vischia <pietro.vischia@gmail.com>
2013-10-14 15:00:28 +0100

    Some renaming for the sake of clarity

--- compileDiagram.sh
+++ compileDiagram.sh
@@ -1,10 +1,10 @@
 # usage: source compileDiagram.sh
-pdflatex diagram_heavyChargedHiggs1
+pdflatex diagram_heavyChargedHiggs_4FS
 mpost fgraphs1
-pdflatex diagram_heavyChargedHiggs1
-pdflatex diagram_heavyChargedHiggs1
+pdflatex diagram_heavyChargedHiggs_4FS
+pdflatex diagram_heavyChargedHiggs_4FS
 
-pdflatex diagram_heavyChargedHiggs2
+pdflatex diagram_heavyChargedHiggs_5FS
 mpost fgraphs2
-pdflatex diagram_heavyChargedHiggs2
-pdflatex diagram_heavyChargedHiggs2
+pdflatex diagram_heavyChargedHiggs_5FS
+pdflatex diagram_heavyChargedHiggs_5FS
